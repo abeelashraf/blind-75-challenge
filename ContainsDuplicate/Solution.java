c3981adc0f4b8eea41d4ce2c84336dc0d1bf6d56
Abeel Ashraf <abeelashraf7@gmail.com>
2025-04-06 10:34:44 +0530

    Added solution for Conatins Duplicate

---
+++ ContainsDuplicate/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> res=new HashSet<>();
+        for(int i:nums){
+            if(res.contains(i)){
+                return true;
+            }
+            res.add(i);
+        }
+        return false;
+        
+    }
+}
